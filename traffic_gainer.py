82f0669140aaf95343b1a653edfec0b3dcd6d677
Khoa Tran <tranduckhoatcu>
2021-07-15 13:14:00 +0700

    add more code

--- traffic_gainer.py
+++ traffic_gainer.py
@@ -5,6 +5,7 @@ from random import randint, sample
 import time
 from nordvpn_switcher import initialize_VPN,rotate_VPN
 import articles
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 
 class WebDriverChrome(object):
 
@@ -37,8 +38,8 @@ class WebDriverChrome(object):
     
     def scroll_bottom(self):
         time.sleep(1)
-        for i in range(20):
-            if (i == 19):
+        for i in range(25):
+            if (i == 23):
                 self.driver.find_element_by_xpath("/html/body/form/div[2]/div[2]/div[3]/div/div[3]/div/div/div/div[2]/div[2]/div/div[4]/div/ul/li[38]/div[3]/a").click()
                 time.sleep(1.5)
                 self.driver.execute_script("window.scrollBy(0,"+str(randint(1000,3000))+");")
@@ -49,7 +50,7 @@ class WebDriverChrome(object):
                 # self.driver.execute_script("window.scrollBy(0,"+str(randint(1000,3000))+");")
             self.driver.execute_script("window.scrollBy(0,"+str(randint(1000,3000))+");")
             time.sleep(randint(2,3))
-        self.driver.execute_script("window.scrollBy(0,"+str(randint(2000,3000))+");")
+        # self.driver.execute_script("window.scrollBy(0,"+str(randint(2000,3000))+");")
     
     def scroll_in_article(self):
         time.sleep(1)
@@ -62,31 +63,31 @@ class WebDriverChrome(object):
             count += rand
             time.sleep(randint(1,2))
         self.driver.execute_script("window.scrollBy(0,"+str(-5000)+");")
+    
+    def load_homepage(self):
+        self.driver.get('https://kenh14.vn/')
+        print('Homepage loaded')
+        self.scroll_down_up()
+        self.driver.find_element_by_xpath('//a[@href="'+self.url_home[0]+'"]').click() # load 'mua xem luon' page
+
+    def article_process(self, article_url):
+        self.scroll_bottom()
+        self.driver.find_element_by_xpath('//a[@href="'+article_url+'"]').click() # get article by url
+        print('Article loaded')
+        self.scroll_in_article()
+        self.driver.find_element_by_xpath('//a[@href="'+self.url_home[0]+'"]').click() #back to 'mua xem luon' page
 
 
     def RunStart(self):
         temp_url = sample(self.url_article , len(self.url_article))
         self.url_article = temp_url
-        self.driver.get('https://kenh14.vn/')
-        print('Page loaded')
-        self.scroll_down_up()
-        # self.driver.find_element_by_xpath("/html/body/form/div[2]/div[2]/div[2]/div[2]/div/div/div/ul/li[7]").click()
-        self.driver.find_element_by_xpath('//a[@href="'+self.url_home[0]+'"]').click()
-        self.scroll_bottom()
-        self.driver.find_element_by_xpath('//a[@href="'+self.url_article[2]+'"]').click()
-        print('Article loaded')
-        self.scroll_in_article()
-        self.driver.find_element_by_xpath('//a[@href="'+self.url_home[0]+'"]').click()
-        self.scroll_bottom()
-        self.driver.find_element_by_xpath('//a[@href="'+self.url_article[1]+'"]').click()
-        print('Article loaded')
-        self.scroll_in_article()
-        self.driver.find_element_by_xpath('//a[@href="'+self.url_home[0]+'"]').click()
-        self.scroll_bottom()
-        self.driver.find_element_by_xpath('//a[@href="'+self.url_article[0]+'"]').click()
-        print('Article loaded')
-        self.scroll_in_article()
-        self.driver.find_element_by_xpath('//a[@href="'+self.url_home[0]+'"]').click()
+        self.load_homepage()
+        
+        for i in range(len(self.url_article)):
+            self.article_process(self.url_article[i])      
+
+
+        self.driver.find_element_by_xpath('//a[@href="'+self.url_home[1]+'"]').click()
         self.scroll_down_up()
         time.sleep(10)
         self.driver.quit()
@@ -94,7 +95,7 @@ class WebDriverChrome(object):
 if __name__ == '__main__':
     command = ['nordvpn','disconnect']
     subprocess.run(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
-    vdisplay = Xvfb(width=800, height=1280)
+    vdisplay = Xvfb(width=1080, height=1920)
     instructions = initialize_VPN(area_input=['Vietnam','Hong Kong','Singapore'], skip_settings=1)
     
     for i in range(20):
@@ -106,13 +107,17 @@ if __name__ == '__main__':
                 continue
             break
         vdisplay.start()
-        Crawl = WebDriverChrome()
-        time.sleep(5)
-        Crawl.RunStart()
-        Crawl = WebDriverChrome()
-        Crawl.RunStart()
-        time.sleep(5)
-        Crawl = WebDriverChrome()
-        Crawl.RunStart()
-        time.sleep(5)
+        try:
+            Crawl = WebDriverChrome()
+            time.sleep(5)
+            Crawl.RunStart()
+            Crawl = WebDriverChrome()
+            Crawl.RunStart()
+            time.sleep(5)
+            Crawl = WebDriverChrome()
+            Crawl.RunStart()
+            time.sleep(5)
+        except (NoSuchElementException, TimeoutException, WebDriverException) as error:
+            print(error)
+            subprocess.run(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
         vdisplay.stop()
