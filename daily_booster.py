b64d47f62f5921138baac736107707776478e8fb
Khoa Tran <tranduckhoatcu>
2021-07-19 13:33:21 +0700

    update code

--- daily_booster.py
+++ daily_booster.py
@@ -125,7 +125,7 @@ if __name__ == '__main__':
                     Crawl.Driver_quit()
                 except:
                     # print(error)
-                    # Crawl.Driver_quit()
+                    Crawl.Driver_quit()
                     continue
                 print('\n')
         vdisplay.stop()
